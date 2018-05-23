fdbe707b9040f10fb4967ac811327f35d95e740e
Dimytrius <dimy.rocha@gmail.com>
2018-05-23 13:13:48 -0300

    new view

---
+++ src/components/Grafico.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import { View, ScrollView, Text, TouchableOpacity, ImageBackground } from 'react-native';
+import { Actions } from 'react-native-router-flux';
+import firebase from 'firebase'
+
+
+class Grafico extends Component {
+    render() {
+        return (
+            <Text>
+                View do Grafico
+            </Text>
+        );
+    }
+}
+export default Grafico
